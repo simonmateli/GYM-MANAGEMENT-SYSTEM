cef84b811707531b2f42ad75b81572c690f2adf2
simonmateli <162945997+simonmateli@users.noreply.github.com>
2026-03-29 00:26:59 +0300

    changes

--- app.py
+++ app.py
@@ -1188,4 +1188,6 @@ def server_error(_):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
